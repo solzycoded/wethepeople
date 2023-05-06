e8a19e357ba1eec4ae85e816631e423b18b74c19
solzycoded <solomonfidelis012@gmail.com>
2023-05-06 17:03:10 +0100

    comment section, DONE!

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,42 @@
+<x-panel>
+    <form method="POST" action="/posts/{{ $post->slug }}/comment">
+        @csrf
+
+        <header class="flex items-center">
+            <img src="https://i.pravatar.cc/60" alt="" width="40" height="40" class="rounded-full">
+
+            <h2 class="ml-3">Want to participate?</h2>
+        </header>
+
+        
+        @auth
+            <div class="mt-6">
+                <textarea 
+                    name="body" 
+                    class="w-full text-sm focus:outline-none focus:ring" 
+
+                    rows="5" 
+                    placeholder="Quick, think of something to say!" 
+                    required></textarea>
+
+                @error('body')
+                    <span class="text-xs text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                <x-submit-button>
+                    Post
+                </x-submit-button>
+            </div>
+        @else
+            <div class="mt-5">
+                <p class="font-semibold">
+                    <a href="/login" class="text-blue-600 hover:underline">Login</a> 
+                    <small>or</small>
+                    <a href="/register" class="text-blue-600 hover:underline">Register</a>
+                </p>
+            </div>
+        @endauth
+    </form>
+</x-panel>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -56,37 +56,8 @@
             <!-- comments -->
             <section class="col-span-8 col-start-5 mt-20 space-y-6">
                 <!-- comments form -->
-                <x-panel>
-                    <form method="POST" action="/posts/{{ $post->slug }}/comment">
-                        @csrf
-
-                        <header class="flex items-center">
-                            <img src="https://i.pravatar.cc/60" alt="" width="40" height="40" class="rounded-full">
-
-                            <h2 class="ml-3">Want to participate?</h2>
-                        </header>
-
-                        
-                        @auth
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Quick, think of something to say!" required></textarea>
-                            </div>
-
-                            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
-                                <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-                        @else
-                            <div class="mt-5">
-                                <p class="font-semibold">
-                                    <a href="/login" class="text-blue-600 hover:underline">Login</a> 
-                                    <small>or</small>
-                                    <a href="/register" class="text-blue-600 hover:underline">Register</a>
-                                </p>
-                            </div>
-                        @endauth
-                    </form>
-                </x-panel>
-
+                @include('posts._add-comment-form')
+                
                 <!-- comments display -->
                 @foreach($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
